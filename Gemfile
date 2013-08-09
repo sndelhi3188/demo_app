5dd1c196e1cb0491afb83f5340ecac24e3134749
Sudheer Nayak <sndelhi3188@gmail.com>
2013-08-09 14:26:26 +0530

    Initail Commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+#ruby-gemset=railstutorial_rails_4_0
+
+gem 'rails', '4.0.0'
+
+source "http://rubygems.org"
+ 
+gem 'httpi', :git => 'git://github.com/bensie/httpi.git', :branch => 'ntlm'
+gem 'savon'
+
+#group :development do
+#  gem 'sqlite3', '1.3.7'
+#end
+
+gem 'mysql', '2.9.1'
+gem 'sass-rails', '4.0.0'
+gem 'uglifier', '2.1.1'
+gem 'coffee-rails', '4.0.0'
+gem 'jquery-rails', '2.2.1'
+gem 'turbolinks', '1.1.1'
+gem 'jbuilder', '1.0.2'
+
+group :doc do
+  gem 'sdoc', '0.3.20', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '0e0c368b3133ea4f98524a1f9489f7357622542d28a1f2bdc61aa1cc132cbf5dd0f82b5fae64d49b7f3a33bb906cf2b5f46745cc6cbbd1d6fc32c5c9e3b94ffd'
